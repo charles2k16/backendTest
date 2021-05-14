532c732227addf87090d8b3e80b8d8f44421f74d
Charles Obeng <charlesobeng000@gmail.com>
2021-05-14 19:05:38 +0000

    added fields

--- models/Shipment.js
+++ models/Shipment.js
@@ -8,6 +8,8 @@ const ShipmentSchema = new mongoose.Schema(
     shipDate: String,
     ship_id: String,
     status: String,
+    description: String,
+    quantity: String,
     createdAt: {
       type: Date,
       default: Date.now
