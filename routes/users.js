95ac30b0c6954c669b997386cdbbde272baf653f
Charles Kwabena <charlesprogram@gmail.com>
2022-11-28 11:02:47 +0000

    remove create

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,14 @@ const express = require('express');
 const {
   getUsers,
   getUser,
-  createUser,
   updateUser,
   login,
 } = require('../controllers/users');
 
 const router = express.Router();
 
-router.route('/').get(getUsers).post(createUser);
+router.route('/').get(getUsers);
+// .post(createUser);
 
 router.get('/login/:passcode/:transaction_code', login);
 router.route('/:id').get(getUser).put(updateUser);
